9c2c7b9493bb4c537230160aea2a4e382bcbdcf1
FaisalAhmedBijoy <faisal.cse16.kuet@gmail.com>
2019-12-02 01:04:27 +0600

    flex practice
    flex new code

---
+++ flex_program/in1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int x1, x2;
+float y1,y2;
+//double m1,m2;
+//double a,b;
+int main()
+{
+
+	printf("%12345\n");
+	printf("123.45\n");
+	/*
+	for (int i = 0; i < count; ++i)
+	{
+		
+		printf("loop %s\n",x1 );
+	}
+	*/
+
+	return 0;
+}
